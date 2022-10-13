7ea191a95d52fb82e890aa73cbf2954fd5a3f439
Ady <80443087+ady00@users.noreply.github.com>
2022-10-13 09:46:28 -0700

    added falconhacks - main file

---
+++ pages/falconhacks.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+import { Calendar } from "../components/countdown";
+import DotMatrix from "../components/homePage";
+import Officers from "../components/prizes";
+import { Square } from "../components/rectangle";
+import { Schedule } from "../components/schedulehacks";
+import { Faq } from "../components/faq";
+
+
+const Home: NextPage = () => {
+  return (
+    <div className="p-4 gap-4">
+      <DotMatrix />
+
+      <div className = "py-2"></div>
+      
+      <div>
+        <Square span="col-span-2" />
+        <div className = "py-2"></div>
+      </div>
+      <div className = "grid grid-cols-1 gap-4 sm:grid-cols-4">
+        <Schedule />
+        <Officers />
+        <Calendar />
+        
+      </div>
+      <div className = "py-4">
+        <Faq span="col-span-2" />
+        <div className = "py-2"></div>
+      </div>
+    </div>
+    
+  );
+};
+
+export default Home;
