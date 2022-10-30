fe5ce964f6c8e6741fe7c1f2cab4eb5d5e7fae8a
arnavg115 <soccercream20@gmail.com>
2022-10-30 05:57:12 +0000

    fixed issue

--- pages/falconhacks.tsx
+++ pages/falconhacks.tsx
@@ -5,31 +5,32 @@ import Officers from "../components/prizes";
 import { Square } from "../components/rectangle";
 import { Schedule } from "../components/schedulehacks";
 import { Faq } from "../components/faq";
-
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    document.body.classList.remove("hid");
+  }, []);
   return (
     <div className="p-4 gap-4">
       <DotMatrix />
 
-      <div className = "py-2"></div>
-      
+      <div className="py-2"></div>
+
       <div>
         <Square span="col-span-2" />
-        <div className = "py-2"></div>
+        <div className="py-2"></div>
       </div>
-      <div className = "grid grid-cols-1 gap-4 sm:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-4">
         <Schedule />
         <Officers />
         <Calendar />
-        
       </div>
-      <div className = "py-4">
+      <div className="py-4">
         <Faq span="col-span-2" />
-        <div className = "py-2"></div>
+        <div className="py-2"></div>
       </div>
     </div>
-    
   );
 };
 
